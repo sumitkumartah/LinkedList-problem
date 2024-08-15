e0530af4193179e57ad3f0cc2a72979cddc819bd
Sumit Kumar Tah <126820502+sumitkumartah@users.noreply.github.com>
2024-08-15 13:02:27 +0530

    Update deleteLL.java

--- Singly LinkedList/deleteLL.java
+++ Singly LinkedList/deleteLL.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 class Node {
-    int data;
-    Node next;
-    Node(int data, Node next) {
-        this.data = data;
-        this.next = next;
+    int data;      // the data value
+    Node next;      // the reference to the next Node in the linked list
+    // Constructors
+    Node(int data1, Node next1) {
+        this.data = data1;  // Initialize data with the provided value
+        this.next = next1;  // Initialize next with the provided reference
     }
-
-    Node(int data) {
-        this.data = data;
-        next = null;
+    Node(int data1) {
+        this.data = data1;  // Initialize data with the provided value
+        this.next = null;   // Initialize next as null since it's the end of the list
     }
 }
 
